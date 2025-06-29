ba1b82a9d39c011fab68dc0076576c43369dcc32
mattlekim <m_brittle@hotmail.com>
2025-06-29 18:15:01 +0100

    start work on steering effects

---
+++ Classes/Vector2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarvinsAIRARefactored.Classes
+{
+    public struct Vector2
+    {
+        public float X; public float Y;
+
+        public Vector2(float value)
+        {
+            this.X = value;
+            this.Y = value;
+        }
+
+        public Vector2(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double Angle()
+        {
+            return Math.Atan2(Y, X);
+        }
+
+        public void Normalize()
+        {
+            float length = (float)Math.Sqrt(X * X + Y * Y);
+            if (length == 0) {
+                X = 0;
+                Y = 0;
+            }
+            X /= length;
+            Y /= length;
+
+        }
+
+        public readonly static Vector2 Zero = new Vector2(0);
+
+        public static bool operator ==(Vector2 left, Vector2 right)
+        {
+            return left.X == right.X && left.Y == right.Y;
+        }
+
+        public static bool operator !=(Vector2 left, Vector2 right)
+        {
+            return left.X != right.X || left.Y != right.Y;
+        }
+
+        public static Vector2 operator -(Vector2 left, Vector2 right)
+        {
+            return new Vector2(left.X - right.X, left.Y - right.Y);
+        }
+
+        public static Vector2 operator +(Vector2 left, Vector2 right)
+        {
+            return new Vector2(left.X + right.X, left.Y + right.Y);
+        }
+    }
+}
---
+++ Components/SteeringEffects.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using MarvinsAIRARefactored.Classes;
+namespace MarvinsAIRARefactored.Components
+{
+    public class SteeringEffects
+    {
+        Vector2 _cChangeInAngle;
+
+        float _carDirection;
+
+        Vector2 _lastDirectionalVector = Vector2.Zero, _currentDirectionalVector = Vector2.Zero;
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void ProccessUndersteer(ref float torque)
+        {
+
+        }
+
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void ProcessEffects(ref float torque)
+        { 
+            //iv jused torque as a ref to avoid garbarge accumalating
+            App app = App.Instance!;
+
+            _lastDirectionalVector = _currentDirectionalVector;
+
+            _currentDirectionalVector.X = app.Simulator.VelocityX;
+            _currentDirectionalVector.Y = app.Simulator.VelocityY;
+
+            if (_lastDirectionalVector == Vector2.Zero)
+            {
+                _lastDirectionalVector = _currentDirectionalVector;
+            }
+
+            _lastDirectionalVector = _currentDirectionalVector - _lastDirectionalVector;
+            _carDirection = (float)_lastDirectionalVector.Angle();
+
+        }
+    }
+}
+
+   
+
