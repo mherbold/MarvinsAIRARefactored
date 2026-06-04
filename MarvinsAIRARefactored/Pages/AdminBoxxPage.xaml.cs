414819e027cb7733d8409a31fb5947edbea66e4d
Marvin Herbold <marvin.herbold@gmail.com>
2026-06-04 15:30:52 -0700

    Fix volume change preview

--- MarvinsAIRARefactored/Pages/AdminBoxxPage.xaml.cs
+++ MarvinsAIRARefactored/Pages/AdminBoxxPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Windows;
+using System.Windows.Threading;
 
 using UserControl = System.Windows.Controls.UserControl;
 
@@ -7,9 +8,14 @@ namespace MarvinsAIRARefactored.Pages;
 
 public partial class AdminBoxxPage : UserControl
 {
+	private readonly DispatcherTimer _volumePreviewTimer = new() { Interval = TimeSpan.FromMilliseconds( 100 ) };
+	private float _pendingVolumePreview = 0f;
+
 	public AdminBoxxPage()
 	{
 		InitializeComponent();
+
+		_volumePreviewTimer.Tick += VolumePreviewTimer_Tick;
 
 #if ADMINBOXX
 		ConnectOnStartup_MairaSwitch.Visibility = Visibility.Collapsed;
@@ -47,9 +53,19 @@ public partial class AdminBoxxPage : UserControl
 
 	private void Volume_ValueChanged( float newValue )
 	{
+		_pendingVolumePreview = newValue;
+
+		_volumePreviewTimer.Stop();
+		_volumePreviewTimer.Start();
+	}
+
+	private void VolumePreviewTimer_Tick( object? sender, EventArgs e )
+	{
 		var app = App.Instance!;
 
-		app.AudioManager.Play( "beep", newValue );
+		_volumePreviewTimer.Stop();
+
+		app.AudioManager.Play( "adminboxx_tone", _pendingVolumePreview );
 	}
 
 	private void Test_Click( object sender, RoutedEventArgs e )
